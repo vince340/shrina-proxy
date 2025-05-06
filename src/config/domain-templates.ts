e8c8415dab9c39ddac48b2479f7a7278e1b476a3
xciphertv <ciphertv@proton.me>
2025-05-05 21:03:36 -0500

    Update domain-templates.ts

--- src/config/domain-templates.ts
+++ src/config/domain-templates.ts
@@ -1042,6 +1042,24 @@ export const domainTemplates: DomainTemplate[] = [
       };
     }
   },
+
+  {
+    pattern: /\.congacdn\.cc$/i,
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0',
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.5',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+    },
+    headersFn: (url: URL) => {
+      return {
+        'origin': 'https://embed.su',
+        'referer': 'https://embed.su/',
+      };
+    }
+  },
   
   // Akamai CDN
   {
